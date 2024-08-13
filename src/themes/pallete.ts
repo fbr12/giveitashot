08ad0c238f922eb1d3eac4a061f362f778ad530a
fbr12 <fabrizzio.ronco@gm2dev.com>
2024-08-13 15:31:54 -0300

    first commit

---
+++ src/themes/pallete.ts
@@ -0,0 +1,14 @@
+export const palette = {
+    primary:{
+        main:'#fff',
+    },
+    background:{
+        main:'#F2F3AE',
+        secondary:'#EDD382',
+        tertiary:'#FC9E4F',
+    },
+    text:{
+        main:'#5E6061',
+    }
+
+}
