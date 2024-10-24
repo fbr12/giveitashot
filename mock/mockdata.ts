bc59f0f222b4bfa2058f2fb743f33311ca49e255
fbr12 <fabrizzio.ronco@gm2dev.com>
2024-10-24 10:28:31 -0300

    feat(mock): create mocking data

---
+++ mock/mockdata.ts
@@ -0,0 +1,61 @@
+import { MovieProps } from "@/modules/MovieCard/MovieCard.component"
+
+
+export const movies:Array<MovieProps> = [
+    {
+        id:0,
+        title:"iceage",
+        img:"/iceage.jpg",
+        description:"muy buena",
+        rate:4
+    },
+    {
+        id:1,
+        title:"madagascar",
+        img:"/madagascar.jpeg",
+        description:"meh",
+        rate:4
+    },
+    {
+        id:2,
+        title:"iceage",
+        img:"/iceage.jpg",
+        description:"muy buena",
+        rate:4
+    },
+    {
+        id:3,
+        title:"madagascar",
+        img:"/madagascar.jpeg",
+        description:"meh",
+        rate:4
+    },
+    {
+        id:4,
+        title:"iceage",
+        img:"/iceage.jpg",
+        description:"muy buena",
+        rate:4
+    },
+    {
+        id:5,
+        title:"madagascar",
+        img:"/madagascar.jpeg",
+        description:"meh",
+        rate:4
+    },
+    {
+        id:6,
+        title:"iceage",
+        img:"/iceage.jpg",
+        description:"muy buena",
+        rate:4
+    },
+    {
+        id:7,
+        title:"madagascar",
+        img:"/madagascar.jpeg",
+        description:"meh",
+        rate:4
+    },    
+]
